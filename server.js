5174d02f1414705f89d9e7f112a7d2e9fc3bebe0
Mubashir150 <mubashirijaz10@gmail.com>
2025-05-09 22:22:00 +0500

    second

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express();
 const {Connect}=require("./db")
+require("dotenv").config();
 
 
 const PORT=3000;
-Connect("mongodb://127.0.0.1:27017/hotels").then(()=>console.log("MongoDB connected")).catch((err)=>console.log("MongoDB Failed", err))
+const mongoUrl=process.env.DB_URL;
+Connect(mongoUrl).then(()=>console.log("MongoDB connected")).catch((err)=>console.log("MongoDB Failed", err))
+
+// const mongourl="mongodb+srv://User1:user1@1@cluster0.6l7px4x.mongodb.net/"
 
 const bodyParser=require("body-parser")
 app.use(bodyParser.json())
